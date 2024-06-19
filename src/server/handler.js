91f2d80bbcd513118880ad8079436811a018bb78
Attaf Riski <atafriski27@gmail.com>
2024-06-19 22:41:54 +0700

    update faster predict

--- src/server/handler.js
+++ src/server/handler.js
@@ -93,66 +93,9 @@ async function roomPricePredictHandler(request, h) {
       id: room.id_property,
     },
   });
-
-  // prediction base model
-  // send the data to the model
-  const basePricePrediction = await HttpML.post("/api/v1/predict-base-price", {
-    unit_type_name: room.unit_type_name,
-    property_design: property.property_design,
-    property_type: property.property_type,
-    number_of_bookings: room.number_of_bookings,
-    bedroom: room.bedroom,
-    bathroom: room.bathroom,
-    beds: room.beds,
-    capacity: room.capacity,
-    ac: room.ac,
-    balcony: room.balcony,
-    beachfront: room.beachfront,
-    breakfast: room.breakfast,
-    building_staff: room.building_staff,
-    cable_tv: room.cable_tv,
-    essentials: room.essentials,
-    garden: room.garden,
-    gym: room.gym,
-    hair_dryer: room.hair_dryer,
-    hanger: room.hanger,
-    heating: room.heating,
-    hot_water: room.hot_water,
-    kitchen: room.kitchen,
-    linens: room.linens,
-    lock: room.lock,
-    luggage_drop_off: room.luggage_drop_off,
-    parking: room.parking,
-    pool: room.pool,
-    private_entrance: room.private_entrance,
-    shampoo: room.shampoo,
-    tv: room.tv,
-    washer: room.washer,
-    wifi: room.wifi,
-    workspace: room.workspace,
-    lat: room.lat,
-    lng: room.lng,
-    distance_to_coastline: room.distance_to_coastline,
-    area_distance_to_airport: room.area_distance_to_airport,
-    property_bedrooms: room.property_bedrooms,
-    area_name: room.area_name,
-    total_fas: room.total_fas,
-    ratio_bedroom_bathroom: room.ratio_bedroom_bathroom,
-    ratio_bedroom_cap: room.ratio_bedroom_cap,
-    avg_price_distance_to_coast: room.avg_price_distance_to_coast,
-    avg_price_distance_to_airport: room.avg_price_distance_to_airport,
-    avg_price_bedroom: room.avg_price_bedroom,
-    avg_price_beds: room.avg_price_beds,
-    avg_price_bathroom: room.avg_price_bathroom,
-    avg_price_total_fas: room.avg_price_total_fas,
-  });
-
-  console.log("base price: "+basePricePrediction.data.data);
-  
-
   // send the data to the model
   const prediction = await HttpML.post("/api/v1/predict-room-price", {
-    average_baseline_price: basePricePrediction.data.data,
+    average_baseline_price: request.payload.base_price,
     rating: room.rating,
     review_sentiment_score: room.review_sentiment_score,
     communication: room.communication,
